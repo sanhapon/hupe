368b44f43690b1198406169ca0cb6c85e0cc5799
sanhapon <sanhapon@yahoo.com>
2022-09-19 22:40:36 +0700

    read yaml configuration

--- server/src/config/configuration.rs
+++ server/src/config/configuration.rs
@@ -1,37 +1,67 @@
 use serde::{Serialize, Deserialize};
 use std::{fs};
 
-
-pub type ConfigurationSection = Vec<ConfigurationElement>;
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct ConfigurationElement {
-    #[serde(rename = "configuration")]
-    configuration: Vec<Configuration>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Configuration {
-    #[serde(rename = "server")]
-    pub server: Server,
+    #[serde(rename = "configuration")]
+    pub servers: Vec<Server>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Server {
-    #[serde(rename = "port")]
-    pub port: i64,
+    #[serde(rename = "listening_port")]
+    pub port: Option<u16>,
 
     #[serde(rename = "enable_retry")]
-    pub enable_retry: bool,
+    pub enable_retry: Option<bool>,
 
     #[serde(rename = "down_streams")]
-    pub down_streams: Vec<String>,
+    pub down_streams: Option<Vec<String>>,
+
+    #[serde(rename = "request_paths")]
+    pub request_paths: Option<Vec<RequestPath>>,
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct RequestPath {
+    #[serde(rename = "path")]
+    pub path: Option<String>,
+
+    #[serde(rename = "map_to")]
+    pub map_to: Option<String>,
+}
+
+
 impl Configuration {
-    pub fn get_config() -> Result<ConfigurationSection, Box<dyn std::error::Error>> {
+    pub fn get_config() -> Result<Configuration, Box<dyn std::error::Error>> {
         let yaml= fs::read_to_string("config.yaml".to_string()).expect("Unnable to find config.yaml");
-        let config: ConfigurationSection = serde_yaml::from_str(&yaml)?;
+        let config: Configuration = serde_yaml::from_str(&yaml)?;
         Ok(config)
+    }
+
+    pub fn save_config() {
+        let rp1 = RequestPath {
+            path: Some("/hotels".to_string()),
+            map_to: Some("/".to_string())
+        };
+        let rp2 = RequestPath {
+            path: Some("/hotels".to_string()),
+            map_to: Some("/".to_string())
+        };
+        let s1 = Server {
+            port: Some(12345),
+            enable_retry: Some(true),
+            down_streams: Some(vec!["www.agoda.com".to_string(), "www.booking.com".to_string()]),
+            request_paths: Some(vec![rp1, rp2])
+        };
+
+        let s2 = s1.clone();
+
+        let c = Configuration {
+            servers: vec![s1, s2]
+        };
+
+        let str = serde_yaml::to_string(&c).unwrap();
+        println!("{}", str);
     }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,72 @@
 mod config;
 
-use  config::configuration::Configuration;
+use hyper::server::conn::AddrStream;
+use hyper::{Body, Request, Response, Server, StatusCode};
+use hyper::service::{service_fn, make_service_fn};
+use std::{convert::Infallible, net::SocketAddr};
+use std::net::IpAddr;
 
-fn main() {
-    println!("Hello, world!");
+use  config::configuration::{Configuration, Server as ConfigServer};
+
+// fn main() {
+//     println!("Hello, world!");
+//     let config = Configuration::get_config().unwrap();
+//     // Configuration::save_config();
+//     println!("{:?}", config);
+// }
+
+
+fn debug_request(req: Request<Body>) -> Result<Response<Body>, Infallible>  {
+    let body_str = format!("{:?}", req);
+    Ok(Response::new(Body::from(body_str)))
+}
+
+fn find_path(req_uri: &http::uri::Uri, config_server: ConfigServer) -> Option<String> {
+    let mut found = false;
+    for (i, config) in config_server.request_paths.unwrap().iter().enumerate() {
+        let str = config.path.unwrap().as_str();
+        if req_uri.path().starts_with(str) {
+            return Some(String::from(str));
+        }
+    }
+    None
+}
+
+async fn handle(client_ip: IpAddr, req: Request<Body>, config_server: ConfigServer) -> Result<Response<Body>, Infallible> {
+    match find_path(req.uri(), config_server) {
+        Some(path) => {
+            match hyper_reverse_proxy::call(client_ip, "http://127.0.0.1:13901", req).await {
+                Ok(response) => {Ok(response)}
+                Err(_error) => {Ok(Response::builder()
+                                  .status(StatusCode::INTERNAL_SERVER_ERROR)
+                                  .body(Body::empty())
+                                  .unwrap())}
+            }
+        },
+        _ => debug_request(req)
+    }
+}
+
+#[tokio::main]
+async fn main() {
     let config = Configuration::get_config().unwrap();
-    println!("{:?}", config);
+
+    let bind_addr = format!("127.0.0.1:{:?}", config.servers[0].port);
+    let addr:SocketAddr = bind_addr.parse().expect("Could not parse ip:port.");
+
+    let make_svc = make_service_fn(|conn: &AddrStream| {
+        let server = config.servers[0].clone();
+        let remote_addr = conn.remote_addr().ip();
+        async move {
+            Ok::<_, Infallible>(service_fn(move |req| handle(remote_addr, req, server)))
+        }
+    });
+
+    let server1 = Server::bind(&addr).serve(make_svc);
+
+    println!("Running server on {:?}", addr);
+
+    if let Err(e) = server1.await {
+        eprintln!("server error: {}", e);
+    }
 }
