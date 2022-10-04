90bbd608ab6033de7d17b7ebf5c1a886c66b1c04
sanhapon <sanhapon@yahoo.com>
2022-10-04 22:23:58 +0700

    disable connection keep alive

---
+++ server/connector.rs
@@ -0,0 +1,107 @@
+use hyper::{Request, Response, Body, Error, Client};
+use hyper_tls::HttpsConnector;
+use regex::Regex;
+
+
+use crate::config::configuration::{Configuration};
+
+#[derive(Debug, Clone)]
+struct RequestMatcher {
+    config_path_regex: Regex,
+    downstream_servers: Vec<String>,
+}
+
+impl RequestMatcher {
+    fn new(regex_pattern: &str, servers: Vec<String>) -> RequestMatcher {
+        println!("New RequestMacher {} {:?}", regex_pattern, servers);
+        RequestMatcher {
+            config_path_regex: Regex::new(regex_pattern).unwrap(),
+            downstream_servers: servers
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Connector {
+    request_matchers: Vec<RequestMatcher>,
+    client: Client<HttpsConnector<hyper::client::HttpConnector>>,
+}
+
+impl Connector {
+    pub fn new(configuration: Configuration) -> Connector {
+        let https = HttpsConnector::new();
+        let client: Client<HttpsConnector<_>, _> = Client::builder()
+        .http2_keep_alive_interval(None)
+        .build(https);
+        
+        let request_matchers: Vec<RequestMatcher> = configuration.server.request_paths
+            .unwrap()
+            .iter()
+            .map(|rp| {
+                let regex_pattern = rp.uri_path.as_ref().unwrap().as_str();
+                let servers = rp.downstreams.as_ref().unwrap().clone();
+                let rm = RequestMatcher::new(regex_pattern, servers);
+                rm
+            }).collect();
+            
+        Connector { client, request_matchers }
+    }
+
+    pub async fn call(&self, mut req: Request<Body>, counter: usize) -> Result<Response<Body>, Error> {
+    
+        let matcher = 
+            self.request_matchers
+            .iter()
+            .find(|r| r.config_path_regex.is_match(req.uri().path()));
+        
+
+        match matcher {
+            Some(m) => {
+                req.strip_headers();
+                let servers = &m.downstream_servers;
+                
+                let index =  counter % servers.len();
+                let host = &servers[index];
+                req.change_to_downstream_host(host.to_string());
+        
+                self.client.request(req).await
+            }, 
+            _ => {
+                let mut resp = Response::new(Body::from(""));
+                *resp.status_mut() = http::status::StatusCode::NOT_FOUND;
+                Ok::<_, Error>(resp)
+            }
+        }
+
+    }
+
+
+    fn mark_down_stream_broken(server_name: String) {
+
+    }
+}
+
+trait RequestFn {
+    fn strip_headers(&mut self);
+    fn change_to_downstream_host(&mut self, host: String);
+}
+
+impl RequestFn for Request<Body> {
+    fn strip_headers(&mut self) {
+        let keys = ["host", "content-length", "transfer-encoding", "accept-encoding", "content-encoding"];
+        for key in keys {
+            self.headers_mut().remove(key);
+        }
+    }
+
+    fn change_to_downstream_host(&mut self, host: String) {
+        let uri = self.uri();
+        let url_string = match uri.query() {
+            None => format!("https://{}{}", host, uri.path()),
+            Some(query) => format!("https://{}{}?{}", host, uri.path(), query)
+        };
+
+        *self.uri_mut() = url_string.parse().unwrap();
+    }
+}
+
---
+++ server/main.rs
@@ -0,0 +1,56 @@
+use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
+
+use hyper::Server;
+use hyper::http::Error;
+use hyper::service::{make_service_fn, service_fn};
+
+mod connector;
+mod config;
+
+
+const MAX_REQ : usize = 18_446_744_073_709_551_000usize;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+
+    let configuration = config::configuration::Configuration::get_config().unwrap();
+    let port = configuration.server.port.unwrap();
+
+    println!("{:?}", configuration);
+
+    let connector = Arc::new(connector::Connector::new(configuration));
+
+    let number = Arc::new(Mutex::new(0usize));
+
+    let make_service = make_service_fn(move |_con| {
+        let connector = connector.clone();
+        let mut number = number.lock().unwrap();
+
+        *number += 1;
+        if *number > MAX_REQ {
+            *number = 0;
+        }
+        
+        let index = *number;
+        
+        async move {
+            Ok::<_, Error>(service_fn(move |req| {
+                let connector = connector.clone();
+
+                async move {
+                    connector.call(req, index).await
+                }
+            }))
+        }
+        
+    });
+
+    let addr = SocketAddr::from(([0,0,0,0], port));
+    Server::bind(&addr)
+        .tcp_keepalive(None)
+        .http1_keepalive(false)
+        .http2_keep_alive_interval(None)
+        .serve(make_service).await.unwrap();
+    Ok(())
+}
